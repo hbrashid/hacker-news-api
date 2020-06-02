541f131fc84152d753c05677d5f2d9fdc8d84922
rodrieri000 <ericarodriguez7@outlook.com>
2020-06-01 21:00:08 -0500

    Erica

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Hit extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLiked: false
+		};
+	}
+
+
+render() {
+  return(
+    <div>
+      <span>{this.props.news.title}</span> <a href={this.props.news.url}>{this.props.news.url}</a>
+      <div>{this.props.news.author}</div>
+      <br></br>
+
+
+
     </div>
-  );
+  )
+}
+}
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      hits: [], 
+      value: ''
+    };
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	componentDidMount() {
+		fetch("http://hn.algolia.com/api/v1/search?query=")
+			.then((json) => json.json())
+			.then((data) => {
+				this.setState({
+					hits: data.hits
+				});
+				console.log(data);
+			})
+			.catch((error) => console.log("parsing failed", error));
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      hits: [], 
+      value: ''      
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields, 
+        ...updatedValue
+      }
+    })
+  }
+
+	render() {
+		return (
+    <div className="App">
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Search:
+          <input type="text" 
+            name="hit"
+            value={this.state.value} 
+            onChange={event => this.handleChange(event)}
+           />
+        </label>
+      </form>
+      {this.state.hits.map((hitData, index) => (
+      <Hit key={index} news={hitData} />
+      ))}
+
+    </div>
+    );
+	}
 }
 
 export default App;
